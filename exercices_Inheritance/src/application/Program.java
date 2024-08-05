b311a6edc549a411c783b027173a79e586de50ee
Maicon Lara <m.lara93@outlook.com>
2024-08-05 13:52:09 -0300

    Final exercicio

--- exercices_Inheritance/src/application/Program.java
+++ exercices_Inheritance/src/application/Program.java
@@ -1,9 +1,65 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n;
+
+		List<Product> list = new ArrayList<>();
+
+		System.out.print("Enter the number of products: ");
+		n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("Product #" + (i + 1) + " data:");
+			System.out.print("Common, used or imported (c/u/i)? ");
+			char ch = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("Nome: ");
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			Double price = sc.nextDouble();
+
+			if (ch == 'c') {
+				list.add(new Product(name, price));
+			} else if (ch == 'u') {
+				sc.nextLine();
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				list.add(new UsedProduct(name, price, date));
+
+			} else {
+				sc.nextLine();
+				System.out.print("Customs fee: ");
+				Double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			}
+
+		}
+
+		System.out.println();
+		System.out.println("PRICE TAGS:");
+		for (Product prod : list) {
+			System.out.println(prod.priceTag());
+		}
+
+		sc.close();
 
 	}
 
